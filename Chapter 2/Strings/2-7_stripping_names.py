ecb3fedb737280f637c8b22f58ac8a7f6b0bbcb5
Richy <richardvarthurs@gmail.com>
2024-07-22 08:38:57 -0400

    more reorg and finished string exercises

---
+++ Chapter 2/Strings/2-7_stripping_names.py
@@ -0,0 +1,12 @@
+#2-7 Page 25
+
+name_with_space = '\n\tMaximus Decimus Meridius\n\t'
+print(name_with_space)
+
+left_strip = name_with_space.lstrip()
+right_strip = name_with_space.rstrip()
+all_strip = name_with_space.strip()
+
+print(f"Left Strip: {left_strip}")
+print(f"Right Strip: {right_strip}")
+print(f"All Strip: {all_strip}")
---
+++ Chapter 2/Strings/2-8_file_extensions.py
@@ -0,0 +1,5 @@
+#2-8 Page 25
+
+file_name = 'python_notes.txt'
+file_name_no_suffix = file_name.removesuffix('.txt')
+print(file_name_no_suffix)
