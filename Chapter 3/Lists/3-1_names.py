5456c78e2882334f3db5ff245041a33b675df067
Richy <richardvarthurs@gmail.com>
2024-07-22 10:03:58 -0400

    Lists first exercises

---
+++ Chapter 3/Lists/3-1_names.py
@@ -0,0 +1,10 @@
+# 3-1 Page 36
+
+clam_castle = ['Richy', 'Brian', 'Lukas', 'Cillian', 'Ronan', 'Mark', 'Jamie']
+print(clam_castle[0])
+print(clam_castle[1])
+print(clam_castle[2])
+print(clam_castle[3])
+print(clam_castle[4])
+print(clam_castle[5])
+print(clam_castle[6])
---
+++ Chapter 3/Lists/3-2_greetings.py
@@ -0,0 +1,12 @@
+# 3-2 Page 36
+
+clam_castle = ['Richy', 'Brian', 'Lukas', 'Cillian', 'Ronan', 'Mark', 'Jamie']
+message = 'You are one of the clams'
+
+print(f'{message}, {clam_castle[0]}')
+print(f'{message}, {clam_castle[1]}')
+print(f'{message}, {clam_castle[2]}')
+print(f'{message}, {clam_castle[3]}')
+print(f'{message}, {clam_castle[4]}')
+print(f'{message}, {clam_castle[5]}')
+print(f'{message}, {clam_castle[6]}')
